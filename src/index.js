6f00741822b23cd51574c7bca7dd5fb73b25b8ab
Josh Butler <joshorion7@gmail.com>
2021-08-27 04:56:03 -0400

    Arrary Desctructuring - useState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function App({name}){
+
+  // Array destructuring for status value
+  const [status, setStatus] = useState("Open");
+
+  return (
+    <div>
+      <h1>Status: {status}</h1>
+      <button conClick={() => setStatus("Closed")}>
+        Closed
+      </button>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
